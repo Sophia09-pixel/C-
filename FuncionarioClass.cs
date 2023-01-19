6695272e29a6ea42c06f94fbf8cf157294b253f4
Sophia de Sousa <68878547+Sophia09-pixel@users.noreply.github.com>
2023-01-19 14:27:52 -0300

    Create FuncionarioClass.cs

---
+++ FuncionarioClass.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Resolvendo3
+{
+    class Funcionario
+    {
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+        public double SalarioLiquido()
+        {
+            return SalarioBruto - Imposto;
+        }
+
+        public void AumentarSalario(double porcentagem)
+        {
+            SalarioBruto = SalarioBruto + (SalarioBruto * porcentagem / 100.0);
+        }
+    }
+}
