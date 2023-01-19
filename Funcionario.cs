709514bba439245b8d535d087e3d16775afbde41
Sophia de Sousa <68878547+Sophia09-pixel@users.noreply.github.com>
2023-01-19 14:27:27 -0300

    Create Funcionario.cs

---
+++ Funcionario.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Globalization;
+namespace Resolvendo3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com os dados do Funcionario:");
+            Funcionario p1 = new Funcionario();
+
+            Console.Write("Nome: ");
+            p1.Nome = Console.ReadLine();
+
+            Console.Write("Salario Bruto: ");
+            p1.SalarioBruto = double.Parse(Console.ReadLine());
+
+            Console.Write("Imposto: ");
+            p1.Imposto = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Funcionario: {p1.Nome}, ${p1.SalarioLiquido().ToString("F2",CultureInfo.InvariantCulture)}");
+            Console.WriteLine();
+
+            Console.Write("Digite a porcentagem para aumentar o salario: ");
+            double porcent = double.Parse(Console.ReadLine());
+            p1.AumentarSalario(porcent);
+
+            Console.WriteLine();
+            Console.Write($"Dados Atualizados: {p1.SalarioLiquido()}");
+
+
+
+
+
+
+
+
+        }
+    }
+}
