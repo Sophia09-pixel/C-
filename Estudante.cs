48d7dd9eeec9caa9258a18ae1ee8c4047500c872
Sophia de Sousa <68878547+Sophia09-pixel@users.noreply.github.com>
2023-01-26 15:18:09 -0300

    Create Estudante.cs

---
+++ Estudante.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Globalization;
+
+namespace Resolvendo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Estudante[] vect = new Estudante[10];
+
+            Console.Write("How many room will be retend? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0;i <n; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Rent #{i}");
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Room: ");
+                int room = int.Parse(Console.ReadLine());
+
+                vect[room] = new Estudante(name,email);
+            }
+
+            Console.WriteLine("Busy rooms:");
+            for (int i = 0; i < 10; i++)
+            {
+                if(vect[i] != null)
+                {
+                    Console.WriteLine(i+ ":" + vect[i]);
+                }
+            }
+
+
+        }
+    }
+}
