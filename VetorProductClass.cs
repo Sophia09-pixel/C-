e24d044b9b1c297a8fa1850a052d8765e2c67fbd
Sophia de Sousa <68878547+Sophia09-pixel@users.noreply.github.com>
2023-01-26 14:36:35 -0300

    Create VetorProductClass.cs

---
+++ VetorProductClass.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Resolvendo2
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Preco { get; set; }
+    }
+}
