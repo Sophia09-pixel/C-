20b1179f98e36c129c88e90ab8d28004399b368a
Sophia de Sousa <68878547+Sophia09-pixel@users.noreply.github.com>
2023-02-16 21:55:37 -0300

    Create file.cs

---
+++ file.cs
@@ -0,0 +1,32 @@
+using System;
+using System.IO;
+
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fs = null;
+            try
+            {
+                fs = new FileStream(@"C:\temp\data.txt", FileMode.Open);
+                StreamReader sr = new StreamReader(fs);
+                string line = sr.ReadLine();
+                Console.WriteLine(line);
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (fs != null)
+                {
+                    fs.Close();
+                }
+            }
+        }
+    }
+}
